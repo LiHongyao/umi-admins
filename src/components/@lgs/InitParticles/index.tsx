4e720e744eaec3acbfcf541f797718cfead8fbb0
LiHONGYAO <lihy_online@163.com>
2024-06-25 16:36:12 +0800

    fix: 升级依赖,更新粒子效果组件 🚀

--- src/components/@lgs/InitParticles/index.tsx
+++ src/components/@lgs/InitParticles/index.tsx
@@ -1,34 +1,39 @@
 // → 安装依赖：pnpm i  react-tsparticles tsparticles-engine tsparticles-slim
 
-import React, { useCallback } from 'react';
-import Particles from 'react-tsparticles';
-import type { Container, Engine } from 'tsparticles-engine';
-import { loadSlim } from 'tsparticles-slim';
+/**
+ * 粒子效果
+ * @See https://github.com/tsparticles/react
+ *
+ * 安装依赖：pnpm add @tsparticles/react @tsparticles/engine @tsparticles/slim
+ */
+import type { Container } from '@tsparticles/engine';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
+import React, { useEffect, useState } from 'react';
 import './index.less';
 import { particlesOptions } from './particlesOptions';
 
 const InitParticles: React.FC = React.memo(() => {
-  const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-    await loadSlim(engine);
+  const [init, setInit] = useState(false);
+  const particlesLoaded = async (container?: Container) => {
+    console.log(container);
+  };
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  const particlesLoaded = useCallback(
-    async (container: Container | undefined) => {
-      await console.log(container);
-    },
-    [],
-  );
-
-  return (
+  return init ? (
     <Particles
-      className={'tsparticles'}
       id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
+      className={'tsparticles'}
+      particlesLoaded={particlesLoaded}
       options={particlesOptions}
     />
-  );
+  ) : null;
 });
 
 export default InitParticles;
--- src/components/@lgs/InitParticles/particlesOptions.ts
+++ src/components/@lgs/InitParticles/particlesOptions.ts
@@ -1,58 +1,54 @@
-import type { ISourceOptions } from 'tsparticles-engine';
+import {
+  MoveDirection,
+  OutMode,
+  type ISourceOptions,
+} from '@tsparticles/engine';
 
 export const particlesOptions: ISourceOptions = {
   background: {
     color: {
-      value: '#EDF0F3',
+      value: '#edf0f3',
     },
   },
-  fpsLimit: 60,
+  fpsLimit: 120,
   interactivity: {
     events: {
       onClick: {
-        enable: true,
+        enable: false,
         mode: 'push',
       },
       onHover: {
         enable: true,
-        mode: 'bubble',
+        mode: 'repulse',
       },
-      resize: true,
     },
     modes: {
-      bubble: {
-        distance: 400,
-        duration: 2,
-        opacity: 0.5,
-        size: 18,
-      },
       push: {
         quantity: 4,
       },
       repulse: {
-        distance: 200,
-        duration: 5,
+        distance: 60,
+        duration: 0.4,
       },
     },
   },
   particles: {
     color: {
-      value: '#87CEFA',
+      value: '#0d47a1',
     },
     links: {
-      color: '#87CEFA',
+      color: '#0d47a1',
       distance: 150,
       enable: true,
-      opacity: 0.3,
+      opacity: 0.5,
       width: 1,
     },
-    collisions: {
-      enable: true,
-    },
     move: {
-      direction: 'none',
+      direction: MoveDirection.none,
       enable: true,
-      outMode: 'bounce',
+      outModes: {
+        default: OutMode.out,
+      },
       random: false,
       speed: 1,
       straight: false,
@@ -60,19 +56,17 @@ export const particlesOptions: ISourceOptions = {
     number: {
       density: {
         enable: true,
-        value_area: 800,
       },
-      value: 20,
+      value: 120,
     },
     opacity: {
-      value: 0.75,
+      value: 0.5,
     },
     shape: {
       type: 'circle',
     },
     size: {
-      random: true,
-      value: 5,
+      value: { min: 1, max: 5 },
     },
   },
   detectRetina: true,
